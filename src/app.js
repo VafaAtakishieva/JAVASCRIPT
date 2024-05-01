3eabafa5add7dad7509c65a4116b2f938fdcaf0f
VafaAtakishieva <vafa.atakishi@gamil.com>
2024-05-01 14:02:21 +0400

    added file

---
+++ src/app.js
@@ -0,0 +1,22 @@
+let m = prompt ("birinci terefi daxil edin");
+let n = prompt ("ikinci terefi daxil edin");
+let sahe = m*n
+alert(sahe);
+
+
+// let a = Number (prompt ("Birinci terefi daxil edin"));
+
+// let b = Number (prompt ("Ikinci terefi daxil edin"));
+
+// let c = Number (prompt ("Ucuncu terefi daxil edin"));
+
+// let hp = (a + b + c) / 2; // hp = halfperimetr
+// let sahe = Math.sqrt(hp*(hp-a)*(hp-b)*(hp-c));
+// alert(sahe);
+
+
+
+
+
+
+
